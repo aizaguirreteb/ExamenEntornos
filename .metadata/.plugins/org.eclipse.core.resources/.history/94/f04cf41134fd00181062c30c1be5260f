61fb4c77ce1743b25074fa07571b2d0459e0fcbd
Alejandra Izaguirre <aleizatebar@gmail.com>
2018-12-11 12:13:06 +0100

    Calculo de Areas: Funciones

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/94/f04cf41134fd00181062c30c1be5260f
@@ -0,0 +1,9 @@
+
+public class CalculoAreas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CalculoDeAreas/src/CalculoAreas.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CalculoAreas {
+	
+	public static void menu() {
+		System.out.println("\tCALCULO DE AREAS");
+		System.out.println();
+		System.out.println("=====================================");
+		System.out.println("  1. Calcular area de un triangulo");
+		System.out.println("  2. Calcular area de un trapecio");
+		System.out.println("  3. Calcular area de un rectangulo");
+		System.out.println("  4. Salir");
+	}
+	
+	public static double calcularAreaTriangulo(double base, double altura) {
+		return (base * altura)/2.0;
+	}
+	
+	public static double calcularAreaRectangulo(double base, double altura) {
+		return base * altura;
+	}
+	
+	public static double calcularAreaTrapecio(double base1, double base2, double altura) {
+		return ((base1 + base2) / 2.0) * altura;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		menu();		
+		
+		
+	}
+
+}
